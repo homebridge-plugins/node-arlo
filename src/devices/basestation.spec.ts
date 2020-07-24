9570d0f46ca6f9a0ebe2a8b43239c52593771491
Kristian Matthews <kristian.matthews@me.com>
2020-07-24 14:24:02 +0100

    Add tests

--- src/devices/basestation.spec.ts
+++ src/devices/basestation.spec.ts
@@ -3,49 +3,61 @@ import { Client } from '../client';
 import { ParentDeviceData, DeviceTypes } from '../responses';
 
 describe(Basestation, () => {
+
+  const client = new Client();
+  const deviceData: ParentDeviceData = {
+    userId: 'USER-ID',
+    deviceId: 'DEVICE-ID',
+    uniqueId: 'UNIQUE-ID',
+    deviceType: DeviceTypes.BASESTATION,
+    deviceName: 'Basestation',
+    lastModified: new Date(),
+    xCloudId: 'X-CLOUD-ID',
+    lastImageUploaded: '',
+    userRole: '',
+    displayOrder: 1,
+    mediaObjectCount: 0,
+    state: '',
+    modelId: 'MODEL-ID',
+    cvrEnabled: true,
+    dateCreated: new Date(),
+    interfaceVersion: 'v0.0.0',
+    interfaceSchemaVer: 'v0.0.0',
+    owner: {
+      firstName: 'Kristian',
+      lastName: 'Matthews',
+      ownerId: 'OWNER-ID'
+    },
+    properties: {
+      modelId: 'MODEL-ID',
+      olsonTimeZone: '',
+      hwVersion: 'v0.0.0'
+    },
+    firmwareVersion: 'v0.0.0',
+    timeZone: '',
+    connectivity: {
+      type: '',
+      connected: false,
+      mepStatus: ''
+    },
+    automationRevision: 0,
+    migrateActivityZone: false
+  };
+  const basestation = new Basestation(client, deviceData);
+
+  it('Should mirror unique ID correctly', () => {
+    expect(basestation.uniqueId).toBe('UNIQUE-ID');
+  });
+
+  it('Should mirror type correctly', () => {
+    expect(basestation.type).toBe(DeviceTypes.BASESTATION);
+  });
+
+  it('Should mirror name correctly', () => {
+    expect(basestation.name).toBe('Basestation');
+  });
     
   it('Should convert to JSON', () => {
-    const client = new Client();
-    const deviceData: ParentDeviceData = {
-      userId: 'USER-ID',
-      deviceId: 'DEVICE-ID',
-      uniqueId: 'UNIQUE-ID',
-      deviceType: DeviceTypes.BASESTATION,
-      deviceName: 'Basestation',
-      lastModified: new Date(),
-      xCloudId: 'X-CLOUD-ID',
-      lastImageUploaded: '',
-      userRole: '',
-      displayOrder: 1,
-      mediaObjectCount: 0,
-      state: '',
-      modelId: 'MODEL-ID',
-      cvrEnabled: true,
-      dateCreated: new Date(),
-      interfaceVersion: 'v0.0.0',
-      interfaceSchemaVer: 'v0.0.0',
-      owner: {
-        firstName: 'Kristian',
-        lastName: 'Matthews',
-        ownerId: 'OWNER-ID'
-      },
-      properties: {
-        modelId: 'MODEL-ID',
-        olsonTimeZone: '',
-        hwVersion: 'v0.0.0'
-      },
-      firmwareVersion: 'v0.0.0',
-      timeZone: '',
-      connectivity: {
-        type: '',
-        connected: false,
-        mepStatus: ''
-      },
-      automationRevision: 0,
-      migrateActivityZone: false
-    };
-    const basestation = new Basestation(client, deviceData);
-
     expect(() => {
       JSON.stringify(basestation)
     }).not.toThrow();
--- src/devices/camera.spec.ts
+++ src/devices/camera.spec.ts
@@ -3,45 +3,57 @@ import { Client } from '../client';
 import { ChildDeviceData, DeviceTypes } from '../responses';
 
 describe(Camera, () => {
+
+  const client = new Client();
+  const deviceData: ChildDeviceData = {
+    userId: 'USER-ID',
+    deviceId: 'DEVICE-ID',
+    uniqueId: 'UNIQUE-ID',
+    deviceType: DeviceTypes.CAMERA,
+    deviceName: 'Camera',
+    lastModified: new Date(),
+    xCloudId: 'X-CLOUD-ID',
+    lastImageUploaded: '',
+    userRole: '',
+    displayOrder: 1,
+    mediaObjectCount: 0,
+    state: '',
+    modelId: 'MODEL-ID',
+    cvrEnabled: true,
+    dateCreated: new Date(),
+    interfaceVersion: 'v0.0.0',
+    interfaceSchemaVer: 'v0.0.0',
+    owner: {
+      firstName: 'Kristian',
+      lastName: 'Matthews',
+      ownerId: 'OWNER-ID'
+    },
+    properties: {
+      modelId: 'MODEL-ID',
+      olsonTimeZone: '',
+      hwVersion: 'v0.0.0'
+    },
+    parentId: 'PARENT-ID',
+    presignedLastImageUrl: 'https://localhost/presigned-last-image',
+    presignedSnapshotUrl: 'https://localhost/presigned-snapshot',
+    presignedFullFrameSnapshotUrl: 'https://localhost/presigned-full-frame-snapshot',
+    arloMobilePlan: false
+  };
+  const camera = new Camera(client, deviceData);
+
+  it('Should mirror unique ID correctly', () => {
+    expect(camera.uniqueId).toBe('UNIQUE-ID');
+  });
+
+  it('Should mirror type correctly', () => {
+    expect(camera.type).toBe(DeviceTypes.CAMERA);
+  });
+
+  it('Should mirror name correctly', () => {
+    expect(camera.name).toBe('Camera');
+  });
     
   it('Should convert to JSON', () => {
-    const client = new Client();
-    const deviceData: ChildDeviceData = {
-      userId: 'USER-ID',
-      deviceId: 'DEVICE-ID',
-      uniqueId: 'UNIQUE-ID',
-      deviceType: DeviceTypes.CAMERA,
-      deviceName: 'Camera',
-      lastModified: new Date(),
-      xCloudId: 'X-CLOUD-ID',
-      lastImageUploaded: '',
-      userRole: '',
-      displayOrder: 1,
-      mediaObjectCount: 0,
-      state: '',
-      modelId: 'MODEL-ID',
-      cvrEnabled: true,
-      dateCreated: new Date(),
-      interfaceVersion: 'v0.0.0',
-      interfaceSchemaVer: 'v0.0.0',
-      owner: {
-        firstName: 'Kristian',
-        lastName: 'Matthews',
-        ownerId: 'OWNER-ID'
-      },
-      properties: {
-        modelId: 'MODEL-ID',
-        olsonTimeZone: '',
-        hwVersion: 'v0.0.0'
-      },
-      parentId: 'PARENT-ID',
-      presignedLastImageUrl: 'https://localhost/presigned-last-image',
-      presignedSnapshotUrl: 'https://localhost/presigned-snapshot',
-      presignedFullFrameSnapshotUrl: 'https://localhost/presigned-full-frame-snapshot',
-      arloMobilePlan: false
-    };
-    const camera = new Camera(client, deviceData);
-
     expect(() => {
       JSON.stringify(camera)
     }).not.toThrow();
